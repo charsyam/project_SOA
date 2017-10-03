91bdb3898df500c29be7f0bf9bcd201d0102686e
Kimgayoung98 <rkdud0520@naver.com>
2017-10-03 23:24:41 +0900

    Windows_History_parse
    using powershell
    sorting date, IE history and file access history
    
    Command:
    
    Get-History | sort-object Date | Export-CSV (Path and Filename) -Encoding UTF8

---
+++ Final_Get-WinHistory(sorting).ps1
@@ -0,0 +1,32 @@
+﻿function Get-History {            
+[CmdletBinding()]            
+param ()            
+            
+
+$shell = New-Object -ComObject Shell.Application          
+$history = $shell.NameSpace(34) 
+$folder = $history.Self       
+            
+$history.Items() |          
+foreach {            
+ if ($_.IsFolder) {           
+   $siteFolder = $_.GetFolder         
+   $siteFolder.Items() |        
+   foreach {            
+     $site = $_             
+     if ($site.IsFolder) {           
+        $pageFolder  = $site.GetFolder         
+        $pageFolder.Items() |        
+        foreach {            
+           $visit = New-Object -TypeName PSObject -Property @{           
+               Place = $($site.Name)           
+               URL = $($pageFolder.GetDetailsOf($_,0))           
+               Date = $( $pageFolder.GetDetailsOf($_,2))     
+           }
+           $visit
+        }            
+     }            
+   }            
+ }            
+}     
+}    
