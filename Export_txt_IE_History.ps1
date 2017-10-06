83a4ecc35a28c9bc4865e7f3e3006de391b77abe
Kimgayoung98 <rkdud0520@naver.com>
2017-10-07 02:00:45 +0900

    export txt_IE history
    change export csv_ IE History
    to export txt_ IE History

---
+++ Export_txt_IE_History.ps1
@@ -0,0 +1,26 @@
+﻿function Get-History {            
+[CmdletBinding()]            
+param ()            
+
+$shell = New-Object -ComObject Shell.Application       
+$history = $shell.NameSpace(34)  
+$folder = $history.Self       
+            
+$history.Items() |             
+foreach {            
+ if ($_.IsFolder) {   
+   $siteFolder = $_.GetFolder     
+   $siteFolder.Items() |       
+   foreach {            
+     $site = $_           
+     if ($site.IsFolder) {   
+        $pageFolder  = $site.GetFolder      
+        $pageFolder.Items() |         
+        foreach {             
+               $($site.Name)+ ' : '+ $($pageFolder.GetDetailsOf($_,0)) + ' : ' + $( $pageFolder.GetDetailsOf($_,2))
+        }            
+     }            
+   }            
+ }            
+}     
+}    
