6e6935a8687de1540c2f324f1ff6e5fd4f0eb366
Kimgayoung98 <rkdud0520@naver.com>
2017-10-08 02:43:56 +0900

    convert timestamp_IE history
    make time convert function

--- Field_Export_txt_IE_History.ps1
+++ Field_Export_txt_IE_History.ps1
@@ -20,8 +20,21 @@ foreach {
         $pageFolder  = $site.GetFolder      
         $pageFolder.Items() |         
         foreach {          
+               $datetime = "$($pageFolder.GetDetailsOf($_,2))"
+               $split_date = $datetime.split(" ")      
+               if($split_date[1] -eq "오후"){
+                $time = $split_date[2].split(":")
+                $add = [int]$time[0] + 12
+                $Fin_Date = $split_date[0] + ' : ' + $add + ':' + $time[1]
+               } elseif($split_date[1] -eq "오전")
+               {
+                    $split_date = $datetime.split(" ")
+                    $time = $split_date[2].split(":")
+                    $Fin_Date = $split_date[0] + ' : ' + $time[0] + ':' + $time[1]
+               }
+               
                $env:userdomain + " : " + $env:COMPUTERNAME + " : " + $($env:hostIP) + ' : ' +    
-               $($env:hostMAC) + ' : ' + $env:username + " : "  + $($pageFolder.GetDetailsOf($_,2)).replace(' ', " : ") + ' : ' +
+               $($env:hostMAC) + ' : ' + $env:username + " : "  + $Fin_Date + ' : ' +
                $($site.Name).replace("내 PC", "") + ' : ' +$($pageFolder.GetDetailsOf($_,0)).replace("file:///", "") + ' : ' + $($pageFolder.GetDetailsOf($_,1))  | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhhmm)_IEhistory.txt -Append
         }            
      }            
